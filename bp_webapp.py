4915617b7beb2fa09436bc5cfcd3906117133d48
Abish Pius <60949830+abishpius@users.noreply.github.com>
2023-03-04 19:15:57 -0500

    Add files via upload

---
+++ bp_webapp.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import numpy as np
+import tensorflow as tf
+# from tensorflow import keras
+# from tensorflow.keras import layers
+# from tensorflow.keras.models import Sequential
+import PIL
+
+import warnings
+warnings.filterwarnings('ignore')
+
+
+
+st.title("Bell's Palsy Identifier")
+
+upload_file = st.file_uploader("Please upload an png of jpg file", type = ['png', 'jpg'])
+
+if upload_file is not None:
+    im = PIL.Image.open(upload_file)
+    rgb_im = im.convert('RGB')
+    rgb_im.save('tmp.jpg')
+    
+    # Load Model
+    model = tf.keras.models.load_model('bp_model_pass1')
+        
+    # Prepare image
+    img = tf.keras.utils.load_img(
+    'tmp.jpg', target_size=(224,224)
+)
+    img_array = tf.keras.utils.img_to_array(img)
+    img_array = tf.expand_dims(img_array, 0)
+    
+    # Print what the top predicted class is
+    preds = model.predict(img_array)
+    score = tf.nn.softmax(preds[0])
+    
+    st.subheader(
+        "Model Predicts: {} with a {:.2f} percent confidence. Actual is {}".format(class_names[np.argmax(score)], 100 * np.max(score), actual)
+  )
